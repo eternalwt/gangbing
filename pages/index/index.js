65bf1ad9a62196cd8a29dccc1fa73d6958883547
eternalwt <eternalwt@qq.com>
2017-09-10 22:28:00 +0800

    use wx:for

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,10 +3,16 @@
 var app = getApp()
 Page({
   data: {
-    priceLst:[],
-    countLst:[],
-    motto: '点击图片购买',
-    userInfo: {},
+    products: [
+      { cid: 0, name: "产品1", image: '../images/0.png', price: app.globalData.price[0], count: app.globalData.count[0] },
+      { cid: 1, name: "产品2", image: '../images/1.png', price: app.globalData.price[1], count: app.globalData.count[1] },
+      { cid: 2, name: "产品3", image: '../images/2.png', price: app.globalData.price[2], count: app.globalData.count[2] },
+      { cid: 3, name: "产品4", image: '../images/3.png', price: app.globalData.price[3], count: app.globalData.count[3] },
+      { cid: 4, name: "产品5", image: '../images/4.png', price: app.globalData.price[4], count: app.globalData.count[4] },
+    ],
+    // name: ["产品1", "产品2", "产品3", "产品4", "产品5"],
+    //priceLst: app.globalData.price,
+    //countLst: app.globalData.count,
     total: 0,
 
     carts: [
@@ -37,24 +43,20 @@ Page({
       
       var that = this
       that.setData({
-        productCount: app.globalData.count[id],
         countLst : app.globalData.count
       })
-      console.log(countLst);
+      console.log(that.countLst);
     }
   },
 
   countAdd: function (event) {
-    //event.stopPropagation;
-    console.log(event);
     var id = event.currentTarget.id;
     console.log(id);
     app.globalData.count[id]++;
 
     var that = this
     that.setData({
-      productCount: app.globalData.count[id],
-      countLst: app.globalData.count
+      'products[id].count' : app.globalData.count[id]
     })
   },
 
@@ -70,13 +72,12 @@ Page({
     //调用应用实例的方法获取全局数据
     
     that.setData({
-      priceLst: app.globalData.price,
-      countLst: app.globalData.count
+      //priceLst: app.globalData.price,
+      //countLst: app.globalData.count
     })
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
-        userInfo:userInfo
       })
     })
   },
@@ -87,6 +88,7 @@ Page({
     that.setData({
       total: app.globalData.totalPrice,
       countLst: app.globalData.count
+      //TODO 应该使用for循环更新数据
     })
   }
 
